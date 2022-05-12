78c7d22f118048647d01af8acce4606bc2f0cc34
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 08:20:09 +0900

    Update setup.py to upload this to package

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-vertical-tab-widget',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='PyQt vertical tab widget (text is horizontal)',
     url='https://github.com/yjg30737/pyqt-vertical-tab-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
